904460b53e4f8d3e4c8fdf9a1ca7e9160a8e55b5
padmini v <padmini.v@perfios.com>
2021-09-01 18:00:45 +0530

    ADDED: operations to perform on binary tree

--- src/com/bridgelabz/binarysearchtree/BinaryTree.java
+++ src/com/bridgelabz/binarysearchtree/BinaryTree.java
@@ -1,5 +1,53 @@
 package com.bridgelabz.binarysearchtree;
 
-public class BinaryTree {
-
+public class BinaryTree<K extends Comparable<K>> 
+{
+	
+	private BinaryTreeNode<K> root;
+	
+	private BinaryTreeNode<K> addRecursively(BinaryTreeNode<K> current, K key)
+	{
+		
+		if(current == null) {
+			return new BinaryTreeNode<K>(key);
+		}
+		
+		int compareResult = key.compareTo(current.key);
+		
+		if(compareResult == 0)
+			return current;
+		if(compareResult < 0)
+			current.left = addRecursively(current.left, key);
+		else
+			current.right = addRecursively(current.right, key);
+		
+		return current;
+		
+	}
+	
+	public void add(K key) 
+	{
+		this.root = this.addRecursively(root, key);
+	}
+	
+	private int getSizeRecursively(BinaryTreeNode<K> current) 
+	{
+		return current == null? 0: 1 + this.getSizeRecursively(current.left)+this.getSizeRecursively(current.right);
+	}
+	
+	public int getSize() 
+	{
+		return this.getSizeRecursively(root);
+	}
+	
+	public void printBinaryTree() 
+	{
+		System.out.println("Binary Tree: " + root);
+	}
+	
+	@Override
+	public String toString() 
+	{
+		return "BinaryTree{" + root + '}';
+	}
 }
