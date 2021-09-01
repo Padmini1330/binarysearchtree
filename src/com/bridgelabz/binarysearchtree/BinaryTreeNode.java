b2859d0be45b3206b901cf8b8e1a9b8255188efb
padmini v <padmini.v@perfios.com>
2021-09-01 17:59:07 +0530

    ADDED: nodes of binary search tree

--- src/com/bridgelabz/binarysearchtree/BinaryTreeNode.java
+++ src/com/bridgelabz/binarysearchtree/BinaryTreeNode.java
@@ -1,5 +1,34 @@
 package com.bridgelabz.binarysearchtree;
 
-public class BinaryTreeNode {
-
+public class BinaryTreeNode<K extends Comparable<K>> 
+{
+	
+	K key;
+	BinaryTreeNode<K> left;
+	BinaryTreeNode<K> right;
+	
+	public BinaryTreeNode(K key) 
+	{
+		this.key = key;
+		this.left = null;
+		this.right = null;
+	}
+	
+	@Override
+	public String toString() 
+	{
+		StringBuilder myNodeString = new StringBuilder();
+		myNodeString.append("{"+"Key = ").append(key).append('}');
+		
+		if(left != null) 
+		{
+			myNodeString.append("->").append(left);
+		}
+		if(right != null) 
+		{
+			myNodeString.append("->").append(right);
+		}
+		return myNodeString.toString();
+	}
+	
 }
